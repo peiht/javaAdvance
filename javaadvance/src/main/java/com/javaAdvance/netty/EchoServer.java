f31b45df0901940f3251576e114b74944b82088f
peiht <1071532941@qq.com>
2020-10-26 23:06:57 +0800

    书上的例子

---
+++ javaadvance/src/main/java/com/javaAdvance/netty/EchoServer.java
@@ -0,0 +1,67 @@
+package com.javaAdvance.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author Administrator
+ *
+ *
+ */
+public class EchoServer {
+
+    private final int port;
+
+    public EchoServer(int port){
+        this.port = port;
+    }
+
+    public void start() throws Exception{
+        final EchoServerHandler serverHandler = new EchoServerHandler();
+        //创建EventLoopGroup
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            //创建 ServerBootstrap
+            ServerBootstrap b = new ServerBootstrap();
+
+            b.group(group)
+                    //指定所使用的NIO传输Channel
+                    .channel(NioServerSocketChannel.class)
+                    //使用指定的端口设置套接字地址
+                    .localAddress(new InetSocketAddress(port))
+                    //添加一个EchoServerHandler到子Channel的ChannelPipeline
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        public void initChannel(SocketChannel ch) {
+                            ch.pipeline().addLast(serverHandler);
+                        }
+                    });
+            //异步的板顶服务器，调用sync()方法阻塞等待直到绑定完成
+            ChannelFuture f = b.bind().sync();
+            //获取Channel的CloseFuture，并且阻塞当前线程直到它完成。
+            f.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //关闭EventLoopGroup。 释放所有资源
+            group.shutdownGracefully().sync();
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        if (args.length != 1) {
+            System.err.println(
+                    "Usage : " + EchoServer.class.getSimpleName() + " <port>"
+            );
+        }
+
+        int port = Integer.parseInt(args[0]);
+        new EchoServer(port).start();
+    }
+}
